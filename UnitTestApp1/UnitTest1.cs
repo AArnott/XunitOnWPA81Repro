8be207329b66b3ab0b976562ddf0099982aff2b3
Andrew Arnott <andarno@microsoft.com>
2015-10-05 14:26:46 -0700

    Add wpa81 unit test project where xunit fails to produce tests in Test Explorer

---
+++ UnitTestApp1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Xunit;
+
+namespace UnitTestApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MSTest()
+        {
+
+        }
+
+        [Fact]
+        public void Xunit()
+        {
+        }
+    }
+}
